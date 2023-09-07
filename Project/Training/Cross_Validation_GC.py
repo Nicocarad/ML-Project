5c4a011807d49aaa5011482c75992ae534f1810f
Nicolò Caradonna <nicocarad@gmail.com>
2023-09-07 13:51:11 +0200

    f

---
+++ Project/Training/Cross_Validation_GC.py
@@ -0,0 +1,294 @@
+import numpy
+import scipy
+import Library.functions as lib
+
+
+
+def mcol(v):
+    return v.reshape((v.size, 1)) # takes a row vector and emit a column vector
+
+def load(fname): 
+    DList = []
+    labelsList = []
+    hLabels = {
+        '0': 0,
+        '1': 1
+        }
+
+    with open(fname) as f:
+        for line in f:
+            try:
+                attrs = line.split(',')[0:12] # list of string ( the first 4 string)
+                attrs = mcol(numpy.array([float(i) for i in attrs])) # list compr
+                name = line.split(',')[-1].strip()
+                label = hLabels[name]
+                DList.append(attrs)
+                labelsList.append(label)
+            except: #if the file contains an empty line at the end
+                pass
+
+    return numpy.hstack(DList), numpy.array(labelsList, dtype=numpy.int32)
+
+
+def mean_and_covariance(data_matrix):
+    N = data_matrix.shape[1]
+    mu = lib.vcol(data_matrix.mean(1)) 
+    DC = data_matrix - mu 
+    C = numpy.dot(DC, DC.T)/N
+    
+    return mu, C
+
+
+
+
+
+def GaussianClassifier(DTR,LTR,DTE,LTE):
+    
+    S = []
+
+    for i in range(LTR.max()+1):
+        D_c = DTR[:,LTR == i] 
+        mu,C = mean_and_covariance(D_c)
+        f_conditional = numpy.exp(lib.logpdf_GAU_ND_fast(DTE, mu, C))
+        S.append(lib.vrow(f_conditional))
+    S = numpy.vstack(S)
+    
+
+    prior = numpy.ones(S.shape)/3.0  
+    SJoint = S*prior
+    SMarginal = lib.vrow(SJoint.sum(0))
+    SPost = SJoint/SMarginal
+     
+    return SPost
+   
+def LogGaussianClassifier(DTR,LTR,DTE,LTE):
+    
+    S = []
+    
+
+    for i in range(LTR.max()+1):
+        D_c = DTR[:,LTR == i] 
+        mu,C = mean_and_covariance(D_c)
+        f_conditional = lib.logpdf_GAU_ND_fast(DTE, mu, C)
+        S.append(lib.vrow(f_conditional))
+    S = numpy.vstack(S)
+    
+    prior = numpy.ones(S.shape)/3.0
+    
+    logSJoint = S + numpy.log(prior)
+    logSMarginal = lib.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    SPost = numpy.exp(logSPost)
+    
+    return SPost
+       
+def NaiveBayes_GaussianClassifier(DTR,LTR,DTE,LTE):
+    
+    S = []
+    
+    for i in range(LTR.max()+1):
+        D_c = DTR[:,LTR == i] 
+        mu,C = mean_and_covariance(D_c)
+        identity = numpy.identity(C.shape[0])
+        C = C*identity
+        f_conditional = lib.logpdf_GAU_ND_fast(DTE, mu, C)
+        S.append(lib.vrow(f_conditional))
+    S = numpy.vstack(S)
+    
+    prior = numpy.ones(S.shape)/2
+    
+   
+    logSJoint = S + numpy.log(prior)
+    logSMarginal = lib.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    llr = logSPost[1,:] - logSPost[0,:] - numpy.log(0.5/0.5)
+
+    return  llr
+
+def TiedGaussianClassifier(DTR,LTR,DTE,LTE):
+    
+    # Calculate the Tied Covariance Matrix
+    C_star = 0 
+    N = DTR.shape[1]
+    for i in range(LTR.max()+1): 
+        D_c = DTR[:, LTR == i] 
+        nc = D_c.shape[1] 
+        C_star = C_star + nc*mean_and_covariance(D_c)[1] 
+    
+    C_star = C_star / N
+      
+    # Apply Gaussian Classifier
+    S = []
+    
+    for i in range(LTR.max()+1):
+        D_c = DTR[:,LTR == i] 
+        mu = mean_and_covariance(D_c)[0]
+        f_conditional = lib.logpdf_GAU_ND_fast(DTE, mu, C_star)
+        S.append(lib.vrow(f_conditional))
+    S = numpy.vstack(S)
+        
+    prior = numpy.ones(S.shape)/3.0
+    
+    logSJoint = S + numpy.log(prior)
+    logSMarginal = lib.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    SPost = numpy.exp(logSPost)
+    
+    return SPost
+    
+def Tied_NaiveBayes_GaussianClassifier(DTR,LTR,DTE,LTE):
+    
+    # Calculate the Tied Covariance Matrix
+    C_star = 0 
+    N = DTR.shape[1]
+    for i in range(LTR.max()+1): 
+        D_c = DTR[:, LTR == i] 
+        nc = D_c.shape[1] 
+        C_star = C_star + nc*mean_and_covariance(D_c)[1] 
+    
+    C_star = C_star / N
+    
+    # Diagonalize the covariance matrix
+    identity = numpy.identity(C_star.shape[0])
+    C_star = C_star*identity
+    
+    # Apply Gaussian Classifier
+    S = []
+    
+    for i in range(LTR.max()+1):
+        D_c = DTR[:,LTR == i] 
+        mu = mean_and_covariance(D_c)[0]
+        f_conditional = lib.logpdf_GAU_ND_fast(DTE, mu, C_star)
+        S.append(lib.vrow(f_conditional))
+    S = numpy.vstack(S)  
+    
+    prior = numpy.ones(S.shape)/3.0
+    
+    logSJoint = S + numpy.log(prior)
+    logSMarginal = lib.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    SPost = numpy.exp(logSPost)
+    
+    
+    return  SPost
+
+
+def kfold(model,k,D,L,seed=0):
+    
+
+    SPost_partial = []
+    folds = []
+     
+     # Create a list with indices of the Label vector in a random order
+    numpy.random.seed(seed)
+    idx = numpy.random.permutation(D.shape[1])
+    
+    Label = L[idx] # randomize the vector of Real_labels in the same way
+
+    
+    fold_size = D.shape[1] // k
+
+    # Divide indices in k-folds
+    for i in range(k):
+        start = i * fold_size
+        end = (i + 1) * fold_size
+        folds.append(idx[start:end])
+    
+
+    # If the number of samples is not divisible by K, add the leavings samples in the last fold
+    if D.shape[1] % k != 0:
+        folds[-1] = numpy.concatenate((folds[-1], idx[k * fold_size:]))
+        
+    # Perform Cross validation
+    for i in range(k):
+        # Choose the i-th fold as validation fold
+        validation_indices = folds[i]
+        DTE = D[:,validation_indices] 
+        LTE = L[validation_indices]
+        # Use the leaving folds as Training Set
+        train_indices = numpy.concatenate([folds[j] for j in range(k) if j != i])
+        DTR = D[:,train_indices] 
+        LTR = L[train_indices]
+        # Append in the list the Scores (posterior probabilities) for the samples of the training fold
+        
+        Spost = model(DTR, LTR, DTE, LTE)
+        
+        print(Spost.shape)
+        SPost_partial.append(Spost)
+       
+          
+    S = numpy.hstack(SPost_partial) 
+      
+    
+    return S,Label
+
+def confusionMatrix(Real,Predicted):
+    
+    K = Real.max()+1 # number of classes
+    confMatrix = numpy.zeros((K,K), dtype=int)
+    
+    for i,j in zip(Predicted,Real):
+        confMatrix[i, j] += 1
+    
+    return confMatrix
+
+def DCF(pi,C_fn,C_fp,confMatrix,type):
+    
+    
+    (TN, FN), (FP, TP) = confMatrix
+    
+    FNR = FN / (FN + TP)
+    FPR = FP / (FP + TN)
+    
+    DCFu = pi*C_fn*FNR + (1-pi)*C_fp*FPR
+    
+    if type == "un-normalized":
+        return DCFu
+    elif type == "normalized":
+        Bdummy = min(pi * C_fn, (1 - pi) * C_fp)
+        DCFn = DCFu/Bdummy
+        return DCFn
+    else:
+        raise ValueError('type must be either "un-normalized" or "normalized"')
+
+def min_DCF(pi, C_fn, C_fp,LTE,scores):
+    
+    t = numpy.concatenate([scores, [-numpy.inf, numpy.inf]])
+    t.sort()
+
+    result = []
+    for i in range(len(t)):
+        
+        Predicted = (scores > t[i]).astype(int)
+        conf_matrix = confusionMatrix(LTE, Predicted)
+        result.append(DCF(pi,C_fn,C_fp,conf_matrix,"normalized"))
+        
+    return min(result)
+
+
+if __name__ == '__main__':
+    
+    
+    #DTR: Training data
+    #LTR: Training labels
+    #DTE: Evaluation data
+    #LTE: Evaluation labels
+    D,L = load("Train.txt")
+    
+    SPost,Label = kfold(NaiveBayes_GaussianClassifier,5,D,L)
+    print(SPost)
+    print(Label.shape)
+    res = min_DCF(0.5,1,1,Label,SPost)
+    print("Min DCF: ", res)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ Project/Utils/PCA.py
@@ -0,0 +1,15 @@
+import numpy
+from Utils.utils import mcol
+
+
+
+def PCA(data_matrix,m):
+    
+    N = data_matrix.shape[1] # number of samples
+    mu = mcol(data_matrix.mean(1)) # data_matrix.mean(1) return a 1-D array so we must transform it into a column
+    DC = data_matrix - mu  # performing broadcast, we are removing from all the data the mean
+    C = numpy.dot(DC, DC.T)/N
+    s, U = numpy.linalg.eigh(C) # compute the eigenvalues and eigenvectors
+    P = U[:, ::-1][:, 0:m] # reverse the matrix in order to move leading eigenvectors in the first "m" column
+    DP = numpy.dot(P.T, data_matrix) ##CHECK IF IT IS CORRECT
+    return DP
