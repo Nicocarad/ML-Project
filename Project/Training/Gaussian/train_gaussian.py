3b54fde78583ec759e65f6be057726162c59ffeb
Nicolò Caradonna <nicocarad@gmail.com>
2023-09-07 19:35:52 +0200

    end day 3

--- Project/Training/Gaussian/train_gaussian.py
+++ Project/Training/Gaussian/train_gaussian.py
@@ -1,34 +1,48 @@
 from Utils.Kfold import *
 from Utils.DCF import min_DCF
 from Training.Gaussian.gaussian import *
+from Utils.PCA import PCA
 
 
 def train_gaussianClassifiers(D, L):
     classifier_list = [
-        # (LogGaussianClassifier, 0.5),
-        # (LogGaussianClassifier, 0.1),
-        # (LogGaussianClassifier, 0.9),
-        # (NaiveBayes_GaussianClassifier, 0.5),
-        # (NaiveBayes_GaussianClassifier, 0.1),
-        # (NaiveBayes_GaussianClassifier, 0.9),
-        # (TiedGaussianClassifier, 0.5),
-        # (TiedGaussianClassifier, 0.1),
-        # (TiedGaussianClassifier, 0.9),
+        (LogGaussianClassifier, 0.5),
+        (LogGaussianClassifier, 0.1),
+        (LogGaussianClassifier, 0.9),
+        (NaiveBayes_GaussianClassifier, 0.5),
+        (NaiveBayes_GaussianClassifier, 0.1),
+        (NaiveBayes_GaussianClassifier, 0.9),
+        (TiedGaussianClassifier, 0.5),
+        (TiedGaussianClassifier, 0.1),
+        (TiedGaussianClassifier, 0.9),
         (Tied_NaiveBayes_GaussianClassifier, 0.5),
         (Tied_NaiveBayes_GaussianClassifier, 0.1),
         (Tied_NaiveBayes_GaussianClassifier, 0.9)
     ]
+    
+    m_list = [11, 10, 9, 8, 7]
 
+    print("GAUSSIAN CLASSIFIERS NO PCA\n")
     for classifier, value in classifier_list:
         SPost, Label = kfold(classifier, 5, D, L, value)
         res = min_DCF(value, 1, 1, Label, SPost)
-        print(f"Min DCF ({value}): ", res)
-        
-        
-        
-        
-def train_Gaussians_PCA(D,L):
+        classifier_name = classifier.__name__
+        print(f"Min DCF ({classifier_name}, {value}): ", {round(res, 3)})
     
+    
+    print("GAUSSIAN CLASSIFIERS + PCA(7-8-9-10-11)\n")
+    for m in m_list:
+        D_pca = PCA(D, m)
+        print(f"Value of m: {m}")   
+        for classifier, value in classifier_list:
+                SPost, Label = kfold(classifier, 5, D_pca, L, value)
+                res = min_DCF(value, 1, 1, Label, SPost)
+                classifier_name = classifier.__name__
+                print(f"Min DCF ({classifier_name}, {value}): ", round(res, 3))
+        
+        
+        
+        
 
 
 
--- Project/main.py
+++ Project/main.py
@@ -32,4 +32,5 @@ if __name__ == '__main__':
     
 # Train Gaussian Classifiers
 train_gaussianClassifiers(D1,L1)
+
     
