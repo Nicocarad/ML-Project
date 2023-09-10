23a8d62c31a9b8a2a4ca8c48c43f44d59628f1d2
Nicolò Caradonna <nicocarad@gmail.com>
2023-09-10 18:35:32 +0200

    f

--- Project/Training/SVM/svm.py
+++ Project/Training/SVM/svm.py
@@ -111,3 +111,54 @@ class PolynomialSvm:
         z = self.LTR * 2 - 1
         self.scores = (
                 mcol(self.alpha) * mcol(z) * ((self.DTR.T.dot(self.DTE) + self.c) ** self.d + self.K ** 2)).sum(0)
+
+
+
+
+
+class RadialKernelBasedSvm:
+
+    def __init__(self, K, C, gamma):
+       
+        self.DTR = 0
+        self.LTR = 0
+        self.DTE = 0
+        self.K = K
+        self.C = C
+        self.gamma = gamma
+        self.alpha = None
+        self.scores = 0
+
+    
+
+    def train(self, DTR, LTR, DTE, eff_prior):
+        self.DTR = DTR
+        self.LTR = LTR
+        self.DTE = DTE
+        self.priors = [eff_prior, 1 - eff_prior]
+        
+        z = 2 * self.LTR - 1
+        kernel_dtr = numpy.zeros((self.DTR.shape[1], self.DTR.shape[1]))
+        for i in range(self.DTR.shape[1]):
+            for j in range(self.DTR.shape[1]):
+                kernel_dtr[i][j] = numpy.exp(
+                    - self.gamma * numpy.linalg.norm(self.DTR[:, i] - self.DTR[:, j]) ** 2) + self.K ** 2
+        h_hat = mcol(z) * mrow(z) * kernel_dtr
+        dual_obj = obj_svm_wrapper(h_hat)
+        alpha, _, _ = fmin_l_bfgs_b(
+            dual_obj,
+            numpy.zeros(self.DTR.shape[1]),
+            bounds=compute_weights(self.C, self.LTR, self.priors),
+            factr=1.0
+        )
+        self.alpha = alpha
+
+    def compute_scores(self):
+        dist = numpy.zeros((self.DTR.shape[1], self.DTE.shape[1]))
+        z = 2 * self.LTR - 1
+        for i in range(self.DTR.shape[1]):
+            for j in range(self.DTE.shape[1]):
+                dist[i][j] += numpy.exp(-self.gamma * numpy.linalg.norm(self.DTR[:, i:i+1] - self.DTE[:, j:j+1]) ** 2) + (self.K)**2
+        self.scores = (mcol(self.alpha) * mcol(z) * dist).sum(0)
+
+    
--- Project/main.py
+++ Project/main.py
@@ -59,7 +59,11 @@ if __name__ == '__main__':
 # Train SVM
 
 #Lin_SVM(D1,L1)
-SVM_RAW_znorm_01(D1, L1, 0.5)
-SVM_RAW_znorm_09(D1, L1, 0.5)
+# SVM_RAW_znorm_01(D1, L1, 0.5)
+# SVM_RAW_znorm_09(D1, L1, 0.5)
 #SVM_diff_priors(D1, L1)
 #SVM_diff_priors_znorm(D1, L1)
+
+Poly_SVM_RAW_znorm_05(D1, L1, 0.5)
+Poly_SVM_RAW_znorm_01(D1, L1, 0.5)
+Poly_SVM_RAW_znorm_09(D1, L1, 0.5)
