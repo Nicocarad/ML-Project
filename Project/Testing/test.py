75be59fc7aed7a6768be95e52834fd96cc5e729a
Nicolò Caradonna <nicocarad@gmail.com>
2023-09-13 22:08:18 +0200

    f

--- Project/Testing/test.py
+++ Project/Testing/test.py
@@ -1,5 +1,5 @@
 
-from Training.GMM.gmm_prova import *
+from Training.GMM.gmm import *
 from Utils.PCA import *
 from Utils.DCF import *
 import numpy as np
@@ -9,15 +9,15 @@ import numpy as np
 
 def GMM_test_best(DTR,DTE,LTR,LTE):
     
-    DTR,DTE = PCA(DTR,12,DTE)
+    DTR,DTE = PCA(DTR,11,DTE)
     
     # DTE = PCA(DTE,12)
     # pca = Pca(12)
     # DTR,DTE = pca.process(DTR,DTE)
     prova = np.load("Testing\GMMDiagonal_PCA_M_12_components_2_alpha_0.1_psi_0.01.npy")
     print(prova)
-    iterations = 1  
-    gmm = GMMDiagonal(iterations)
+    iterations = 2  
+    gmm = GMM(iterations)
     gmm.train(DTR,LTR,DTE,LTE,None)
     gmm.compute_scores()
     scores = gmm.scores
