b035108890981e83281d3b79a5f51be480f87347
Nicolò Caradonna <nicocarad@gmail.com>
2023-09-13 22:09:22 +0200

    fin

--- Project/Testing/test.py
+++ Project/Testing/test.py
@@ -10,12 +10,6 @@ import numpy as np
 def GMM_test_best(DTR,DTE,LTR,LTE):
     
     DTR,DTE = PCA(DTR,11,DTE)
-    
-    # DTE = PCA(DTE,12)
-    # pca = Pca(12)
-    # DTR,DTE = pca.process(DTR,DTE)
-    prova = np.load("Testing\GMMDiagonal_PCA_M_12_components_2_alpha_0.1_psi_0.01.npy")
-    print(prova)
     iterations = 2  
     gmm = GMM(iterations)
     gmm.train(DTR,LTR,DTE,LTE,None)
